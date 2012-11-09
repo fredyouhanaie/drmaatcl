54e70475fc868ee8e851df4e9db52998d5dbb173
Fred Youhanaie <fred-gh@anydata.co.uk>
2012-11-09 15:24:52 +0000

    DRY KISS

--- Tests/run-testsuite.tcl
+++ Tests/run-testsuite.tcl
@@ -1,7 +1,7 @@
 
-# test-1.tcl
+# run-testuite.tcl
 #	complete test suite for drmaatcl
-#	run via drmaash
+#	run this via drmaash
 #
 
 package require tcltest
@@ -9,185 +9,62 @@ tcltest::configure -verbose bpstel
 
 source drmaa-testsuite.tcl
 
-tcltest::test ST_MULT_INIT {Multiple drmaa_init} {
-	-body {ST_MULT_INIT}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
+# wrapper for all tests
+proc runtest {tname args} {
+	if [llength $args] {
+		set alist [join $args "_"]
+		set test_name "${tname}_${alist}"
+	} else {
+		set test_name $tname
+	}
+	# now that we have a decent test name, run it
+	tcltest::test "$test_name" "$tname ($args)" {
+		-body {$tname {*}$args}
+		-cleanup {catch drmaa::drmaa_exit}
+		-returnCodes ok
+	}
 }
 
-tcltest::test ST_MULT_EXIT {Multiple drmaa_exit} {
-	-body {ST_MULT_EXIT}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
+runtest ST_MULT_INIT
+runtest ST_MULT_EXIT
+runtest ST_SUPPORTED_ATTR
+runtest ST_SUPPORTED_VATTR
+runtest ST_VERSION
+runtest ST_DRM_SYSTEM
+runtest ST_DRMAA_IMPL
+runtest ST_CONTACT
+
+runtest ST_EMPTY_SESSION_SYNCHRONIZE_DISPOSE
+runtest ST_EMPTY_SESSION_SYNCHRONIZE_NODISPOSE
+
+foreach ctrl_op {HOLD RELEASE SUSPEND RESUME TERMINATE} {
+	runtest ST_EMPTY_SESSION_CONTROL $ctrl_op
 }
 
-tcltest::test ST_SUPPORTED_ATTR {Supported attributes} {
-	-body {ST_SUPPORTED_ATTR}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
+runtest ST_BULK_SUBMIT_WAIT
+runtest ST_SUBMIT_WAIT
 
-tcltest::test ST_SUPPORTED_VATTR {Supported vector attributes} {
-	-body {ST_SUPPORTED_VATTR}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
+runtest ST_BULK_SINGLESUBMIT_WAIT_INDIVIDUAL
 
-tcltest::test ST_VERSION {DRMAA version} {
-	-body {ST_VERSION}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
+runtest ST_SUBMITMIXTURE_SYNC_ALL_DISPOSE
+runtest ST_SUBMITMIXTURE_SYNC_ALL_NODISPOSE
+runtest ST_SUBMITMIXTURE_SYNC_ALLIDS_DISPOSE
+runtest ST_SUBMITMIXTURE_SYNC_ALLIDS_NODISPOSE
 
-tcltest::test ST_DRM_SYSTEM {DRM system} {
-	-body {ST_DRM_SYSTEM}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
+runtest ST_EXIT_STATUS
+runtest ST_SUBMIT_KILL_SIG
 
-tcltest::test ST_DRMAA_IMPL {DRMAA implementation} {
-	-body {ST_DRMAA_IMPL}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
+runtest ST_INPUT_FILE_FAILURE
+runtest ST_OUTPUT_FILE_FAILURE
+runtest ST_ERROR_FILE_FAILURE
 
-tcltest::test ST_CONTACT {Contact string} {
-	-body {ST_CONTACT}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
+runtest ST_SUBMIT_IN_HOLD_RELEASE
+runtest ST_SUBMIT_IN_HOLD_DELETE
 
-tcltest::test ST_EMPTY_SESSION_WAIT {Wait for empty session} {
-	-body {ST_EMPTY_SESSION_WAIT}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test ST_EMPTY_SESSION_SYNCHRONIZE_DISPOSE {synchronize with empty session (dispose)} {
-	-body {ST_EMPTY_SESSION_SYNCHRONIZE_DISPOSE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test ST_EMPTY_SESSION_SYNCHRONIZE_NODISPOSE {synchronize with empty session (nodispose)} {
-	-body {ST_EMPTY_SESSION_SYNCHRONIZE_NODISPOSE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test ST_EMPTY_SESSION_CONTROL {Control empty session} {
-	-body {foreach action HOLD RELEASE RESUME SUSPEND TERMINATE {ST_EMPTY_SESSION_CONTROL $action}}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test ST_BULK_SUBMIT_WAIT {Submit bulk jobs and wait for completion (single-thread)} {
-	-body {ST_BULK_SUBMIT_WAIT}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test ST_SUBMIT_WAIT {Submit jobs and wait for completion (single-thread)} {
-	-body {ST_SUBMIT_WAIT}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_BULK_SINGLESUBMIT_WAIT_INDIVIDUAL {Submit bulk/single jobs, wait individually (single-thread)} {
-	-body {ST_BULK_SINGLESUBMIT_WAIT_INDIVIDUAL}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_SUBMITMIXTURE_SYNC_ALL_DISPOSE {Submit bulk/single jobs, sync all, dispose (single-thread)} {
-	-body {ST_SUBMITMIXTURE_SYNC_ALL_DISPOSE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_SUBMITMIXTURE_SYNC_ALL_NODISPOSE {Submit bulk/single jobs, sync all, wait individually (single-thread)} {
-	-body {ST_SUBMITMIXTURE_SYNC_ALL_NODISPOSE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_SUBMITMIXTURE_SYNC_ALLIDS_DISPOSE {Submit bulk/single jobs, sync all ids, dispose (single-thread)} {
-	-body {ST_SUBMITMIXTURE_SYNC_ALLIDS_DISPOSE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_SUBMITMIXTURE_SYNC_ALLIDS_NODISPOSE {Submit bulk/single jobs, sync all ids, wait individually (single-thread)} {
-	-body {ST_SUBMITMIXTURE_SYNC_ALLIDS_NODISPOSE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_EXIT_STATUS {Submit single jobs, check exit status} {
-	-body {ST_EXIT_STATUS}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_SUBMIT_KILL_SIG {Submit single jobs, kill, check termination} {
-	-body {ST_SUBMIT_KILL_SIG}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_INPUT_FILE_FAILURE {Submit single job with invalid input path} {
-	-body {ST_INPUT_FILE_FAILURE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_OUTPUT_FILE_FAILURE {Submit single job with invalid output path} {
-	-body {ST_OUTPUT_FILE_FAILURE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_ERROR_FILE_FAILURE {Submit single job with invalid error path} {
-	-body {ST_ERROR_FILE_FAILURE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_SUBMIT_IN_HOLD_RELEASE {Submit job in hold, then release and check} {
-	-body {ST_SUBMIT_IN_HOLD_RELEASE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_SUBMIT_IN_HOLD_DELETE {Submit job in hold, then terminate and check} {
-	-body {ST_SUBMIT_IN_HOLD_DELETE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_BULK_SUBMIT_IN_HOLD_SESSION_RELEASE {Submit bulk job in hold, then release and check} {
-	-body {ST_BULK_SUBMIT_IN_HOLD_SESSION_RELEASE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_BULK_SUBMIT_IN_HOLD_SESSION_DELETE {Submit bulk job in hold, then terminate and check} {
-	-body {ST_BULK_SUBMIT_IN_HOLD_SESSION_DELETE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_BULK_SUBMIT_IN_HOLD_SINGLE_RELEASE {Submit bulk job in hold, then release and check} {
-	-body {ST_BULK_SUBMIT_IN_HOLD_SINGLE_RELEASE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
-
-tcltest::test  ST_BULK_SUBMIT_IN_HOLD_SINGLE_DELETE {Submit bulk job in hold, then terminate and check} {
-	-body {ST_BULK_SUBMIT_IN_HOLD_SINGLE_DELETE}
-	-cleanup {catch drmaa::drmaa_exit}
-	-returnCodes ok
-}
+runtest ST_BULK_SUBMIT_IN_HOLD_SESSION_RELEASE
+runtest ST_BULK_SUBMIT_IN_HOLD_SESSION_DELETE
+runtest ST_BULK_SUBMIT_IN_HOLD_SINGLE_RELEASE
+runtest ST_BULK_SUBMIT_IN_HOLD_SINGLE_DELETE
 
 puts "\n\n"
 tcltest::cleanupTests
